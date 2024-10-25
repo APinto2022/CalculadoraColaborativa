2496ff37f2bb18d14144e0da8fd6d083f9a61d11
Maximiliano Herrera <herreramaximiliano@hotmail.com>
2024-10-25 20:12:17 -0300

    archivo potencia

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const sumar=require("./sumar.js")
 const dividir=require("./dividir.js")
+const potencia=require("./potencia.js")
 
 let resultado=sumar(1,5)
 console.log(resultado)
 
 console.log(dividir(10,2))
+console.log(potencia(5,2))
