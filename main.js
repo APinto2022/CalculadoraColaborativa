a6707cfcf5fe51dbb776dc4b1cf7368bfedb28f7
rubisgonzalez <juniorubis00@gmail.com>
2024-10-29 20:19:20 -0300

    funcion multiplicar

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const sumar=require("./sumar.js")
 const dividir=require("./dividir.js")
 const potencia=require("./potencia.js")
+const multiplicar = require("./multiplicar.js")
 
 let resultado=sumar(1,5)
 console.log(resultado)
 
 console.log(dividir(10,2))
 console.log(potencia(5,2))
+console.log(multiplicar(5,2))
